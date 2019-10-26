5fea07e43d3fa4ffea0e568d88119babd7e469f7
Danny <danny@militaryservices.co>
2019-10-26 10:47:13 -0500

    Setup p5 Canvas with shapes

---
+++ app.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(640,  480);
+
+}
+function draw () {
+    noStroke();
+    fill(0,255,0);
+    rect(320, 240, 20,  20);
+    
+fill(255,0,0);
+circle(500, 100, 20)
+
+stroke(0,0,255);
+strokeWeight(3);
+line(0, 40, 640, 40);
+}
